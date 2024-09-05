9da26eb6785654dd5fdc6ae6e420e3bd3a87f107
LiYaoYu <1252505184@qq.com>
2024-09-05 14:59:14 +0800

    实现分页组件，学习了组件事件、全局样式、实例成员$emit

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,23 +1,30 @@
 <template>
   <div>
     <h1>App组件</h1>
-    <Icon type="kaixin" />
+    <Pager :total="total" :current="current" @pageChange="handlePageChange" />
   </div>
 </template>
 
 <script>
-import Icon from "./components/Icon.vue";
+import Pager from "./components/Pager";
 
 export default {
   components: {
-    Icon,
+    Pager,
+  },
+  data() {
+    return {
+      current: 20,
+      total: 302,
+    };
+  },
+  methods: {
+    handlePageChange(newPage) {
+      console.log(newPage);
+      this.current = newPage;
+    },
   },
 };
 </script>
 
-<style scoped>
-.iconfont {
-  font-size: 26px;
-  color: red;
-}
-</style>
+<style scoped></style>
