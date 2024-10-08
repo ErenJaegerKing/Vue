5a88ae09595153663e8d676381cb647b9f93a58f
ErenJaeger <ErenJaegerKing@qq.com>
2024-10-08 10:27:04 +0800

    vue2：初始化项目，element-ui组件库入门到实战，element-admin-template的基础模板

---
+++ mysite-client/src/components/Loading/test.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Loading v-if="isLoading" />
+    <button @click="isLoading=!isLoading">切换显示/隐藏</button>
+  </div>
+</template>
+
+<script>
+import Loading from "@/components/Loading";
+export default {
+  components: {
+    Loading,
+  },
+  data() {
+    return {
+      isLoading: true,
+    };
+  },
+};
+</script>
+
+<style></style>
