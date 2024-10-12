6443d39f04747d3a0535550009ad7052702bab76
ErenJaeger <ErenJaegerKing@qq.com>
2024-10-13 00:45:49 +0800

    vue2:组件库 首页标语part2 完成编辑和图片上传

--- background-system/src/api/banner.js
+++ background-system/src/api/banner.js
@@ -1,9 +1,18 @@
-import request from '@/utils/request'
+import request from "@/utils/request";
 
 // 获取首页标语
 export function getBanner(data) {
   return request({
-    url: '/api/banner',
-    method: 'get',
-  })
+    url: "/api/banner",
+    method: "get",
+  });
+}
+
+// 设置首页标语
+export function setBanner(data) {
+  return request({
+    url: "/api/banner",
+    method: "post",
+    data,
+  });
 }
--- background-system/src/views/banner/index.vue
+++ background-system/src/views/banner/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="app-container">
+    <!-- 数据表格 -->
     <el-table :data="data" style="width: 100%" border>
       <el-table-column prop="date" label="序号" width="60">
         <template slot-scope="scope">
@@ -40,22 +41,63 @@
               icon="el-icon-edit"
               circle
               size="mini"
+              @click="editBannerHandle(scope.row)"
             ></el-button>
           </el-tooltip>
         </template>
       </el-table-column>
     </el-table>
+    <!-- 编辑首页标语 -->
+    <el-dialog title="请编辑信息" :visible.sync="dialogFormVisible" top="5vh">
+      <el-form :model="form">
+        <el-form-item label="标题">
+          <el-input v-model="form.title"></el-input>
+        </el-form-item>
+
+        <el-form-item label="描述">
+          <el-input v-model="form.description"></el-input>
+        </el-form-item>
+        <el-row>
+          <el-col :span="12">
+            <el-form-item label="首页中图">
+              <Upload v-model="form.midImg" />
+            </el-form-item>
+          </el-col>
+          <el-col :span="12">
+            <el-form-item label="首页中图">
+              <Upload v-model="form.bigImg" />
+            </el-form-item>
+          </el-col>
+        </el-row>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="editBannerConfirm">确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
 <script>
-import { getBanner } from "@/api/banner";
+import { getBanner, setBanner } from "@/api/banner";
 import { server_URL } from "@/urlConfig";
+import Upload from "../../components/upload.vue";
 export default {
   data() {
     return {
       data: [],
+      dialogFormVisible: false, // 编辑对话框是否显示
+      form: {
+        id: "",
+        midImg: "",
+        bigImg: "",
+        title: "",
+        description: "",
+      },
     };
+  },
+  components: {
+    Upload,
   },
   created() {
     this.fetchData();
@@ -69,6 +111,28 @@ export default {
           item.midImg2 = server_URL + item.midImg;
           item.bigImg2 = server_URL + item.bigImg;
         }
+      });
+    },
+    editBannerHandle(bannerInfo) {
+      // 将数据给form 打开dialog
+      this.form = { ...bannerInfo };
+      this.dialogFormVisible = true;
+    },
+    // 保存更改的数据
+    editBannerConfirm() {
+      let arr = [...this.data];
+      for (var i = 0; i < arr.length; i++) {
+        if (arr[i].id == this.form.id) {
+          arr[i] = this.form;
+        }
+      }
+      setBanner(arr).then((res) => {
+        this.dialogFormVisible = false;
+        this.$message({
+          message: "修改成功",
+          type: "success",
+        });
+        this.fetchData();
       });
     },
   },
